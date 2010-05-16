88bd3f157f962226d1c98fa0b54180444d9b28e2
regebro <regebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-16 15:35:36 +0000

    Get the version from version.txt.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = open(os.path.join("collective", "blog", "feeds", "version.txt")).read().strip()
 
 setup(name='collective.blog.feeds',
       version=version,
