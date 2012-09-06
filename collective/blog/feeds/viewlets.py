7066fc6cc5e2dd30c231eb3912a5e662dfae13ad
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-09-06 15:39:20 +0200

    use index instead of render to call the template. This way, you can override it, if needed, using z3c.jbot

--- collective/blog/feeds/viewlets.py
+++ collective/blog/feeds/viewlets.py
@@ -2,4 +2,4 @@ from plone.app.layout.viewlets import ViewletBase
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
 
 class FeedsViewlet(ViewletBase):
-    render = ViewPageTemplateFile('feedsviewlet.pt')
+    index = ViewPageTemplateFile('feedsviewlet.pt')
