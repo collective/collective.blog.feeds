31986f268439d763226c01e3f0bca86526b01d10
Lennart Regebro <regebro@gmail.com>
2013-05-26 19:47:49 +0200

    Update versions

--- setup.py
+++ setup.py
@@ -11,7 +11,13 @@ setup(name='collective.blog.feeds',
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         ],
       keywords='plone blog rss podcast feeds',
       author='Jarn AS',
